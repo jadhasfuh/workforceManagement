c2ab03dfd640871e4be37b4758242adeffd8ac54
jadhasfuh <64806015+jadhasfuh@users.noreply.github.com>
2023-08-07 21:06:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-from fastapi import FastAPI, HTTPException
+from flask import Flask, request
 from pymongo import MongoClient
-from pydantic import BaseModel
+from bson.json_util import dumps
 
-app = FastAPI()
-
-# http://127.0.0.1:8000/createWorkOrder
-# pip install fastapi uvicorn pymongo
+#pip install Flask
+app = Flask(__name__)
 
 # Replace 'your_mongodb_uri' with your actual MongoDB connection string
 client = MongoClient('mongodb+srv://Adrian:bEyiBP4PsllpsylS@cluster0.wvmdahm.mongodb.net/?retryWrites=true&w=majority')
-
 # Replace 'your_database_name' with your desired database name
 db = client.wfm
-
 # Replace 'your_collection_name' with your desired collection name
 collection = db.orders
 
-class Order(BaseModel):
-    Producto: str
-    Domicilio: str
-    Contacto: str
-    FechaAgendada: str
-    NumOrdenServicio: str
-    TipoMovimientoFacilidades: str
-    TipoModem: str
-    TipoTecnologia: str
-
-
-@app.post("/createWorkOrder")
-def createWorkOrder(order: Order):
+# Create a new item
+@app.route('/createWorkOrder', methods=['POST'])
+def create_item():
     try:
-        # Create a new instance of the Item model
-        new_item = Order(**order.dict())
+        data = request.get_json()
+        # Borramos antigua data
+        db.items.delete_many({})
         # Insert the new_item into the collection
-        insert_result = collection.insert_one(new_item.dict())
+        insert_result = collection.insert_one(data)
         # Get the inserted document's ID
         inserted_id = str(insert_result.inserted_id)
         # Create the response message
-        response_message = f"Orden {order.NumOrdenServicio} creada"
+        response_message = f"Orden {data.get('NumOrdenServicio')} creada"
         # Return the response
         return {"message": response_message, "inserted_id": inserted_id}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return {"Error al insertar": e}
+
+# Get all items
+@app.route('/', methods=['GET'])
+def get_items():
+    try:
+        return dumps(list(collection.find()))
+    except Exception as e:
+        return {"Error al insertar": e}
